b42b362074821fc786104a03c91944328da06972
Ranganrs1 <49313240+Ranganrs1@users.noreply.github.com>
2020-08-24 20:41:12 +0000

    Deploying to gh-pages from  @ e893cdbac55839bb5687cbb0176fa9e28271f399 🚀

---
+++ precache-manifest.e6480ce1edc8b8e76f617e643aa680c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bedb9e0df670a5dc9ec5a75207ebd630",
+    "url": "/ACNHMacrosJoyConDroid/index.html"
+  },
+  {
+    "revision": "830d1cf537514a24f0c5",
+    "url": "/ACNHMacrosJoyConDroid/static/css/main.cb666d61.chunk.css"
+  },
+  {
+    "revision": "bd14b2a9c37292bd6358",
+    "url": "/ACNHMacrosJoyConDroid/static/js/2.852cfc9a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ACNHMacrosJoyConDroid/static/js/2.852cfc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "830d1cf537514a24f0c5",
+    "url": "/ACNHMacrosJoyConDroid/static/js/main.c8c2a475.chunk.js"
+  },
+  {
+    "revision": "8a6f14992a9afad1d37b",
+    "url": "/ACNHMacrosJoyConDroid/static/js/runtime-main.f208a6e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ACNHMacrosJoyConDroid/precache-manifest.35fcc2e9ed9857e266e586c954940d1e.js"
+  "/ACNHMacrosJoyConDroid/precache-manifest.e6480ce1edc8b8e76f617e643aa680c6.js"
 );
 
 self.addEventListener('message', (event) => {
