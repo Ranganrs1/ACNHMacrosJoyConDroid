320a000404c2f67b76274a36a8ab07638f259b27
Ranganrs1 <49313240+Ranganrs1@users.noreply.github.com>
2020-08-29 17:46:02 +0000

    Deploying to gh-pages from  @ 3b6f16496b2ea880055407ddd7a2d62737aba8dc 🚀

---
+++ precache-manifest.dc900e3e9d03351f1bf899a41ab1e188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39801aeea66b9e1fd5f4bce923c0c71f",
+    "url": "/ACNHMacrosJoyConDroid/index.html"
+  },
+  {
+    "revision": "3ad4bb80361a178b95d9",
+    "url": "/ACNHMacrosJoyConDroid/static/css/main.3f366c6b.chunk.css"
+  },
+  {
+    "revision": "499eaf2d8bcb3bfca828",
+    "url": "/ACNHMacrosJoyConDroid/static/js/2.3327ca52.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ACNHMacrosJoyConDroid/static/js/2.3327ca52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad4bb80361a178b95d9",
+    "url": "/ACNHMacrosJoyConDroid/static/js/main.d429a8e8.chunk.js"
+  },
+  {
+    "revision": "8a6f14992a9afad1d37b",
+    "url": "/ACNHMacrosJoyConDroid/static/js/runtime-main.f208a6e7.js"
+  }
+]);
